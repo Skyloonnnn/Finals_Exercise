54ee5ebee4cdfbd5e387b85bc08a5e6d410ac124
My Laptop <My Laptop@DESKTOP-0U8QNKN>
2025-10-15 11:53:25 +0800

    Poymorphism

---
+++ Polymophism/Polymophism/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymophism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shoe running = new RunningShoe
+            {
+                Brand = "New Balance",
+                Model = "FuelCell Rebel v5",
+                Size = 9.5
+            };
+
+            Shoe basketball = new BasketShoe
+            {
+                Brand = "Nike",
+                Model = "Nike Book 1",
+                Colorway = "White"
+            };
+
+            running.DisplayInfo();
+            basketball.DisplayInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Polymophism/Polymophism/Shoe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymophism
+{
+    class Shoe
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Model: {Model}");
+        }
+
+    }
+
+    class RunningShoe : Shoe
+    {
+        public double Size { get; set; }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Running Shoe Information");
+            base.DisplayInfo();
+            Console.WriteLine($"Size: {Size}\n");
+        }
+    }
+
+    class BasketShoe : Shoe
+    {
+        public string Colorway { get; set; }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("BasketBall Shoe Information");
+            base.DisplayInfo();
+            Console.WriteLine($"Color: {Colorway}");
+        }
+    }
+}
